bd702cb226e34d6573d22ef262524f2045a88ae7
Aijana Schumann <aijana.schumann@gmail.com>
2017-06-09 15:51:38 +0200

    setup page and viewmodel, dbservice

---
+++ Spendingz/Spendingz/Spendingz/Services/IDbStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spendingz.Services
+{
+    public interface IDbStorage
+    {
+        bool CreateDatabase<T>(T dataType);
+
+        bool CreateOrUpdateEntry<T>(T data);
+
+        bool DeleteEntry<T>(T data);
+    }
+}
--- Spendingz/Spendingz/Spendingz/ViewModels/SetupPageViewModel.cs
+++ Spendingz/Spendingz/Spendingz/ViewModels/SetupPageViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Spendingz.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,100 @@ namespace Spendingz.ViewModels
 {
     public class SetupPageViewModel : ViewModelBase
     {
+        private string _newCategoryTitle;
+        private ObservableCollection<Category> _demoCategories;
+
+        private RelayCommand<Category> _deleteCategory;
+
+        private RelayCommand _createCategory;
+
+        private RelayCommand _saveCategories;
+
+        private RelayCommand _dontSave;
+
+        public string NewCategoryTitle {
+            get
+            {
+                return _newCategoryTitle;
+            }
+            set
+            {
+                _newCategoryTitle = value;
+                RaisePropertyChanged(nameof(NewCategoryTitle));
+            }
+        }
+
+
+
+        public ObservableCollection<Category> DemoCategories {
+            get { return _demoCategories; }
+            set {
+                _demoCategories = value;
+                RaisePropertyChanged(nameof(DemoCategories));
+            }
+        }
+
+        public SetupPageViewModel()
+        {
+            
+            DemoCategories = new ObservableCollection<Category> { new Category { Title = "Lebensmittel" }, new Category { Title="Kino" }, new Category { Title = "Reisen" } };
+
+        }
+
+        public RelayCommand<Category> DeleteCatagory
+        {
+            get
+            {
+                if (_deleteCategory == null)
+                {
+                    _deleteCategory = new RelayCommand<Category>( (obj) => 
+                    {
+                        DemoCategories.Remove(obj);
+                    });
+                    return _deleteCategory;
+                }
+                else { return _deleteCategory; }
+            }
+        }
+
+        public RelayCommand CreateCategory
+        {
+            get
+            {
+                if (_createCategory == null)
+                {
+                    _createCategory = new RelayCommand(()=> {
+                        if(!string.IsNullOrEmpty(NewCategoryTitle))
+                        {
+                            DemoCategories.Add(new Category { Title = NewCategoryTitle });
+                            NewCategoryTitle = string.Empty;
+                        }
+                    });
+                    return _createCategory;
+                }
+                else { return _createCategory; }
+            }
+        }
+
+        public RelayCommand SaveCategories
+        {
+            get
+            {
+                if (_saveCategories == null)
+                {
+                    _saveCategories = new RelayCommand(()=> 
+                    {
+                        //TODO: save categories to db and nav
+                    });
+                    return _saveCategories;
+                }
+                else
+                {
+                    return _saveCategories;
+                }
+            }
+        }
+
+        
     }
 }
