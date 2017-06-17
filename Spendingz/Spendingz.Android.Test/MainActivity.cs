71b3e7453ce301b75ba208a173e37da1a6c4f84e
Aijana Schumann <aijana.schumann@gmail.com>
2017-06-17 12:31:21 +0200

    unit test project

---
+++ Spendingz/Spendingz.Android.Test/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+using Android.Content;
+
+namespace Spendingz.Android.Test
+{
+    [Activity(Label = "Spendingz.Android.Test", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        public static Context Context { get; private set; }
+        protected override void OnCreate(Bundle bundle)
+        {
+            Context = this;
+
+            // tests can be inside the main assembly
+            AddTest(Assembly.GetExecutingAssembly());
+           
+            // or in any reference assemblies
+            // AddTest (typeof (Your.Library.TestClass).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
+
---
+++ Spendingz/Spendingz.Android.Test/PlattformServicesTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+using Spendingz.Droid.Services;
+using Spendingz.Model.Data;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Spendingz.Android.Test
+{
+    [TestFixture]
+    public class PlattformServicesTest
+    {
+        public DbStorage DbStorage;
+        public LocalStorage LocalStorage;
+        public string Folder;
+        [SetUp]
+        public void Setup() {
+
+            DbStorage = new DbStorage();
+            LocalStorage = new LocalStorage(MainActivity.Context);
+            Folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            DbStorage.CreateDatabase<Category>();
+
+        }
+
+
+        [TearDown]
+        public void Tear()
+        {
+            File.Delete(Path.Combine(Folder, "Category.db"));
+        }
+
+        [Test]
+        public void LocalStorageTest()
+        {
+            LocalStorage.SaveBool("Tested",true);
+            var actual= LocalStorage.GetBool("Tested");
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void LocalStorageTestSpellingMistake()
+        {
+            LocalStorage.SaveBool("Tested", true);
+            var actual = LocalStorage.GetBool("Testeded");
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void DbStorageExists()
+        {
+            var actual = File.Exists(Path.Combine(Folder,"Category.db"));
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void DbStorageInsert()
+        {
+          
+            var data = new Category { Description = "test Running", Title = "Testeloni" };
+            var actual = DbStorage.CreateOrUpdateEntry(data);
+            Assert.IsTrue(actual!=null && actual.Value!=0);
+        }
+
+        [Test]
+        public void DbStorageInsertMultiple()
+        {
+           
+            var list = new List<Category> { new Category { Title="test 1" }, new Category { Title="Test 2" } };
+            var actual = DbStorage.CreateEntries(list);
+            Assert.IsTrue(actual.Count==2);
+        }
+
+        [Test]
+        public void DbStorageDelete()
+        {
+            var data = new Category { Description = "test Running", Title = "Testeloni" };
+            var id = DbStorage.CreateOrUpdateEntry(data);
+            data.Id = id.Value;
+            var actual = DbStorage.DeleteEntry(data);
+            Assert.IsTrue(actual);
+        }
+    }
+}
